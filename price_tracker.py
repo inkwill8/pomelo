954b0dc62189f640644bfe900431523f2bc385a6
inkwill8 <inkwill8@proton.me>
2026-07-06 16:02:00 -0400

    test(main): test instantiation of comp class

--- price_tracker.py
+++ price_tracker.py
@@ -1,6 +1,9 @@
-import urls
+import urls as u
 import components as cp
 
-
+cpu = cp.Component('Ryzen 9 9950X3D', u.urls[0]["newegg_url"], cp.ComponentType.cpu)
+newegg_price = cpu.get_newegg_price(cpu.url, u.headers)
+cpu.set_newegg_price(newegg_price)
+print(cpu.name + ' ' + cpu.type + ' ' + cpu.newegg_price)
 
 
