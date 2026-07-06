7af5edf20e1f8b9bd947573cf9be0c974161fab5
inkwill8 <inkwill8@proton.me>
2026-07-06 16:17:26 -0400

    build(component): basic object instantiation

--- components.py
+++ components.py
@@ -16,6 +16,10 @@ class ComponentType(Enum):
     hdd = "HDD"
     fan = "Fan"
 
+    def type_to_str(self, type):
+        if type is self.cpu:
+            return "CPU"
+
 
 class Component:
     def __init__(self, name: str, url: str, type: ComponentType):
--- price_tracker.py
+++ price_tracker.py
@@ -4,6 +4,7 @@ import components as cp
 cpu = cp.Component('Ryzen 9 9950X3D', u.urls[0]["newegg_url"], cp.ComponentType.cpu)
 newegg_price = cpu.get_newegg_price(cpu.url, u.headers)
 cpu.set_newegg_price(newegg_price)
-print(cpu.name + ' ' + cpu.type + ' ' + cpu.newegg_price)
+print(cpu.name + ' ' + cpu.type.value + ' ')
+print(cpu.newegg_price)
 
 
